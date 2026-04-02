8335d110d3a69a1fb451c89193b670a19e84dcba
DiegoCreator <marcinm30510@gmail.com>
2026-04-02 10:00:00 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,85 @@
+import { findPathTyped } from './pathfinder';
+import { drawGrid } from './renderer';
+
+const GRID_SIZE = 50;
+const grid = new Uint8Array(GRID_SIZE * GRID_SIZE)
+
+const canvas = document.getElementById('myCanvas') as HTMLCanvasElement | null;
+let startIdx: number | null = null;
+let endIdx: number | null = null;
+
+const btnStartBfs = document.getElementById('runBfs');
+const btnReset = document.getElementById('resetBfs');
+let isDrawing =  false;
+let clickCount = 0;
+
+if (!canvas) {
+        throw new Error("Canvas element not found!")
+    }
+
+canvas.addEventListener('mousedown', (event) => {
+    isDrawing = true;
+
+    handleInput(event)
+});
+
+window.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+
+
+
+canvas.addEventListener('mousemove', (event) => {
+    if (isDrawing && clickCount >= 2) {
+        handleInput(event);
+    }
+});
+
+function handleInput(event: MouseEvent) {
+    if (!canvas) {
+        throw new Error("Canvas element not found!")
+    }
+
+    const rect = canvas.getBoundingClientRect();
+    const cellSize = canvas.width / GRID_SIZE;
+    const x = Math.floor((event.clientX - rect.left) / cellSize);
+    const y = Math.floor((event.clientY - rect.top) / cellSize);
+    const index = y * GRID_SIZE + x;
+
+   
+    if (index < 0 || index >= grid.length) return;
+
+    if (grid[index] === 0) {
+        if (clickCount === 0) {
+            grid[index] = 2;
+            startIdx = index;
+            clickCount++;
+        } else if (clickCount === 1) {
+            grid[index] = 3; 
+            endIdx = index;
+            clickCount++;
+        } else {
+            grid[index] = 1; 
+        }
+        drawGrid(grid, GRID_SIZE);
+    }
+}
+
+btnStartBfs?.addEventListener('click', () => {
+    if (startIdx !== null && endIdx !== null) {
+        console.log(findPathTyped(startIdx, endIdx, grid))
+        drawGrid(grid, GRID_SIZE)
+    } else {
+        alert("You must first mark the Start and Finish lines on the board!")
+    }
+});
+
+btnReset?.addEventListener('click', () => {
+    grid.fill(0);
+    clickCount = 0;
+    startIdx = null;
+    endIdx = null;
+    drawGrid(grid, GRID_SIZE);
+});
+
+drawGrid(grid, GRID_SIZE)
---
+++ src/renderer.ts
@@ -0,0 +1,53 @@
+export function drawGrid(grid: Uint8Array, gridSize: number) {
+    const canvas = document.getElementById('myCanvas') as HTMLCanvasElement | null;
+
+    if (!canvas) {
+        throw new Error("Canvas element not found!")
+    }
+
+    const ctx = canvas.getContext('2d')
+
+    if (!ctx) {
+        throw new Error("Canvas element not found!")
+    }
+
+    const cellSize = canvas.width / gridSize
+
+    ctx.strokeStyle = 'red'
+    ctx.lineWidth = 1;
+
+    ctx.beginPath();
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i <= grid.length; i++) {
+        if (grid[i] !== 0) {
+            const x = (i % gridSize) * cellSize
+            const y = Math.floor(i / gridSize) * cellSize;
+
+            if (grid[i] === 1) ctx.fillStyle = "#333";
+            if (grid[i] === 2) ctx.fillStyle = "#2ECC71";
+            if (grid[i] === 3) ctx.fillStyle = "#E74C3C";
+            if (grid[i] === 4) ctx.fillStyle = "#F1C40F";
+
+            ctx.fillRect(x + 1, y + 1, cellSize - 2, cellSize - 2);
+        }   
+    }
+
+    if (gridSize <= 1000) {
+        ctx.beginPath();
+        ctx.strokeStyle = 'red'
+        ctx.lineWidth = 0.5;
+
+        for (let i = 0; i <= gridSize; i++) {
+            const pos = i * cellSize;
+
+            ctx.moveTo(pos, 0);
+            ctx.lineTo(pos, canvas.height);
+
+            ctx.moveTo(0, pos);
+            ctx.lineTo(canvas.width, pos)
+        }
+        ctx.stroke();
+    }
+}
